fdf081e06ef9746033f9421f60af2e3dff4049da
chessed03 <chessed03@gmail.com>
2023-05-16 16:23:16 -0600

    init stable

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        laravel([
+            'resource/css/app.css',
+            'resource/js/app.js'
+        ]),
+    ],
+});
+
+/*export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});*/
